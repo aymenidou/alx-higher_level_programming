e46bfcb1742beeedb08a44464c04ee7ae40a670a
Aymen Idouahman <aymanidou@gmail.com>
2023-08-10 22:58:48 +0100

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(26, 0, -1):
     if (i % 2 == 0):
-        print(f"{chr(96 + i)}", end="")
+        print("{:s}".format(chr(96 + i)), end="")
     else:
-        print(f"{chr(64 + i)}", end="")
+        print("{:s}".format(chr(64 + i)), end="")
