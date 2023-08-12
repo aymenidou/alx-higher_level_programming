24a8a6c101dcf54df2370bc38513faf322c822c1
Aymen Idouahman <aymanidou@gmail.com>
2023-08-12 01:59:47 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,13 @@
 if __name__ == "__main__":
     import sys
     len = len(sys.argv) - 1
+
     if (len == 0):
         print("{} argument.".format(len))
     elif (len == 1):
         print("{} argument:".format(len))
     else:
         print("{} arguments:".format(len))
+
     for i in range(1, len + 1):
         print("{}: {}".format(i, sys.argv[i]))
