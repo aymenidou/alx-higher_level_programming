66ee47376ae1362a40f77b1842302b08984572b5
Aymen Idouahman <aymanidou@gmail.com>
2023-08-14 23:19:41 +0100

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    result = ""
+    for i in range(len(my_string)):
+        if (my_string[i] not in "Cc"):
+            result += my_string[i]
+    return result
