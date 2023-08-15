94b6dd3a73b0e8733646776c6e02d31e42bcf42f
Aymen Idouahman <aymanidou@gmail.com>
2023-08-15 21:06:49 +0100

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    sort =  sorted(name for name in names if not name.startswith("_"))
+    for name in sort:
+        print(name)
