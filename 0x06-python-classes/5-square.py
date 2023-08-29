303bcc91866cda38fce2f33af32bdb435122c3aa
Aymen Idouahman <aymanidou@gmail.com>
2023-08-29 20:28:33 +0100

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -38,6 +38,7 @@ class Square:
             self.__size = value
 
     def my_print(self):
+        """print square"""
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#",end="")
