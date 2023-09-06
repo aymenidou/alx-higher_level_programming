d4a37b6c4e504a7e547c364bf812c563e9b8f7ac
Aymen Idouahman <aymanidou@gmail.com>
2023-09-06 11:00:19 +0100

    3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """defining rectangle"""
+
+    @property
+    def width(self):
+        """retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set value of width"""
+        if (not isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set value of height"""
+        if (not isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def __init__(self, width=0, height=0):
+        """init method"""
+        self.height = height
+        self.width = width
+
+    def area(self):
+        """area method"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """perimeter method"""
+        if (self.width == 0 or self.height == 0):
+            return 0
+        return (2 * self.width) + (2 * self.height)
+
+    # def __repr__(self):
+    #     """print square"""
+    #     if (self.width == 0 or self.height == 0):
+    #         print()
+    #     else:
+    #         for i in range(self.__size):
+    #             for j in range(self.__size):
+    #                 print("#", end="")
+    #             print()
+
+    def __str__(self):
+        """print square"""
+        if (self.width == 0 or self.height == 0):
+            print()
+        else:
+            for i in range(self.height):
+                for j in range(self.width):
+                    print("#", end="")
+                if (i == self.height - 1):
+                    return ""
+                print()
