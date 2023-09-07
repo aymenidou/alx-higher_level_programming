335b25993c39f4381e4ae9169f8b99f9e7489aea
Aymen Idouahman <aymanidou@gmail.com>
2023-09-07 09:40:32 +0100

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -40,8 +40,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """init method"""
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def area(self):
