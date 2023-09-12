850d09aab812289286c56b2281b67f297280b47e
Aymen Idouahman <aymanidou@gmail.com>
2023-09-12 22:43:49 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
     f.closed
