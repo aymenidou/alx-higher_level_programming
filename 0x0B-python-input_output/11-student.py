7244c0fdb3fd53eddd18ea6cb3a42605350de90d
Aymen Idouahman <aymanidou@gmail.com>
2023-09-12 23:42:35 +0100

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""module for 0x0B-python-input_output"""
+
+
+
+class Student:
+    """Public instance attributes:
+    first_name
+    last_name
+    age"""
+    def __init__(self, first_name, last_name, age):
+        """init object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return self.__dict__
+        else:
+            d = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    d[i] = self.__dict__[i]
+            return d
+    
+    def reload_from_json(self, json):
+        """reload_from_json"""
+        for i in json.keys():
+            self.__dict__[i] = json[i]
