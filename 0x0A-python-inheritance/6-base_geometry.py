1afe08e64c596090d16e61f424026aabbb6641ea
Aymen Idouahman <aymanidou@gmail.com>
2023-09-12 14:47:22 +0100

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module conatining empty class"""
+
+
+class BaseGeometry:
+    """class BaseGeometry with one function
+    """
+    def area(self):
+        """function raising an exception"""
+        raise Exception("area() is not implemented")
