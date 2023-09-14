f5f6c5cd0101a9b00c879ce70258dcdfd9cfd8fd
Aymen Idouahman <aymanidou@gmail.com>
2023-09-14 13:15:22 +0100

    101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(mc, name, value):
     """function that adds a new attribute to an object if it’s possible"""
-    if (hasattr(mc, "__dict__")):
+    if (not hasattr(mc, "__dict__")):
         raise TypeError("can't add new attribute")
     else:
         setattr(mc, name, value)
