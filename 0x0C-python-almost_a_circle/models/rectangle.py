667873600859284473aac8e1fa84f48b46e92f84
Aymen Idouahman <aymanidou@gmail.com>
2023-09-18 23:21:16 +0100

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,7 +83,8 @@ class Rectangle(Base):
     def __str__(self):
         """return rectangle info"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                        self.__x, self.__y, self.__width, self.__height)
+                                                       self.x, self.y,
+                                                       self.width, self.height)
 
     def update(self, *args, **kwargs):
         """update rectangle"""
