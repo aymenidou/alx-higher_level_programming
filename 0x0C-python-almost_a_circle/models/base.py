952006b887d79f95156fadf5a5938c64d21a44ca
Aymen Idouahman <aymanidou@gmail.com>
2023-09-18 15:43:54 +0100

    17. JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,6 +15,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if (list_dictionaries is None):
@@ -30,3 +31,10 @@ class Base:
                 array.append(obj.to_dictionary())
         with open(cls.__name__ + '.json', 'w') as f:
             f.write(cls.to_json_string(array))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """from_json_string"""
+        if (json_string is None):
+            return ""
+        return json.loads(json_string)
