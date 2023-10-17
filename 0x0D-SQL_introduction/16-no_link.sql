da9a98d034765abb87ad4ae73a3670b7f7ad02c4
Aymen Idouahman <aymanidou@gmail.com>
2023-10-18 00:14:38 +0100

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- display records with name not null
-SELECT score, name from second_table WHERE name != '' ORDER BY score desc;
+SELECT score, name FROM second_table WHERE name != '' ORDER BY score desc;
