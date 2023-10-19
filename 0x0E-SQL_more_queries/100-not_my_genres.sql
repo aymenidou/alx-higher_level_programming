143778793c89281bd64e61a313150f3f0265256b
Aymen Idouahman <aymanidou@gmail.com>
2023-10-20 00:26:44 +0100

    100-not_my_genres.sql 101-not_a_comedy.sql 102-rating_shows.sql 103-rating_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,2 @@
--- Go to UTF8
-USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT DISTINCT name FROM tv_genres g WHERE g.id NOT IN (SELECT genre_id FROM tv_show_genres sg LEFT JOIN tv_shows s ON s.id = sg.show_id WHERE s.title = 'Dexter') ORDER BY g.name ASC;
--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,2 +1,2 @@
--- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+-- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT DISTINCT title FROM tv_shows s WHERE s.id NOT IN (SELECT show_id FROM tv_show_genres sg INNER JOIN tv_genres g ON g.id = sg.genre_id WHERE g.name = 'Comedy') ORDER BY title ASC;
