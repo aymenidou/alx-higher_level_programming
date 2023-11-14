7ccfeb59da6736fb5cb8f4958c00901b74c7ee23
Aymen Idouahman <aymanidou@gmail.com>
2023-11-14 23:36:06 +0100

    fix char issue

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,17 +6,13 @@ class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (c === undefined) {
-      super.print();
-    } else {
-      for (let i = 0; i < this.width; i++) {
-        let line = '';
-        for (let j = 0; j < this.width; j++) {
-          line += c;
-        }
-        console.log(line);
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += c;
       }
+      console.log(line);
     }
   }
 }
