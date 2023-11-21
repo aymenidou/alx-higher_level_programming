179be9cef39e6dcc42d027657345b8993d1a9ee1
Aymen Idouahman <aymanidou@gmail.com>
2023-11-21 12:36:28 +0100

    9. Contains

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module execute sql query using SQLAlchemy module"""
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import Session
+
+
+def main(argv):
+    """function containing the connection and executing the query"""
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).where(State.name.like("%a%"))\
+            .order_by(asc(State.id)).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 4):
+        main(sys.argv)
