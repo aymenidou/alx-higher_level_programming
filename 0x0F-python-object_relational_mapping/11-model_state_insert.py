287c0d02c7c24fe4ccfa91e14bd1b6eac1bd42bd
Aymen Idouahman <aymanidou@gmail.com>
2023-11-21 12:58:42 +0100

    11. Add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module execute sql query using SQLAlchemy module"""
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import Session
+
+
+def main(argv):
+    """function containing the connection and executing the query"""
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).order_by(desc(State.id)).first()
+    if (state is None):
+        print('Nothing')
+    else:
+        print("{}".format(state.id))
+    session.close()
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 4):
+        main(sys.argv)
