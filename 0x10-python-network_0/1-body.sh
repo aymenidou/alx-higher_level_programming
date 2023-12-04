bafee63a80cfdffbd832dc472429d4d0fb815da8
Aymen Idouahman <aymanidou@gmail.com>
2023-12-04 11:34:32 +0100

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # get response_code check if it's 200 then display the body of the page
-response_code=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ $response_code -eq 200 ] && curl -s "$1"
+response_code=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ $response_code -eq 200 ] && curl -s "$1" || echo "Error: Received status code $response_code"
