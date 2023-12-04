94653d1ba4774593d7cce7ce6e91d66146ad765d
Aymen Idouahman <aymanidou@gmail.com>
2023-12-04 13:51:04 +0100

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send post request to url in argument with file as param
-curl -s -L -X POST "$1" --form "fileupload=@$2"
+curl -s -L -X POST "$1" -d "@$2" -H "Content-Type: application/json"
