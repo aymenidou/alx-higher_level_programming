59604ef991d295147c108f38331ef5c2a5593928
Aymen Idouahman <aymanidou@gmail.com>
2024-01-04 23:13:36 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,5 +2,9 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (err, res, body) {
-  console.log(JSON.parse(body).title);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
