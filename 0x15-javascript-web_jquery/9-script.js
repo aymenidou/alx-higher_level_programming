ba68fd2e21140ce46479b5f0f9df6c5e86fd84e3
Aymen Idouahman <aymanidou@gmail.com>
2024-01-11 01:28:31 +0100

    9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-$.get('https://hellosalut.stefanbohacek.dev/?lang=fr', (data, status) => {
-  if (status === 'success') {
-    $('DIV#hello').text(data.hello);
-  }
+$(document).ready(() => {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', (data, status) => {
+    if (status === 'success') {
+      $('DIV#hello').text(data.hello);
+    }
+  });
 });
