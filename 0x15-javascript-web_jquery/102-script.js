c886c23242a82d445b21e1d174236d82a81049a3
Aymen Idouahman <aymanidou@gmail.com>
2024-01-23 11:49:16 +0100

    102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,6 +1,6 @@
-
+#!/usr/bin/node
 const URL = 'https://hellosalut.stefanbohacek.dev/';
-
+const $ = window.$;
 function SayHello (lang) {
   $.get(`${URL}?lang=${lang}`, (data, status) => {
     if (status === 'success' && lang) {
@@ -13,5 +13,12 @@ $(document).ready(function () {
   $('#btn_translate').click(() => {
     const lang = $('#language_code').val();
     SayHello(lang);
+  });
+  $('#language_code').keypress(function (event) {
+    const keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode === 13) {
+      const lang = $('#language_code').val();
+      SayHello(lang);
+    }
   });
 });
