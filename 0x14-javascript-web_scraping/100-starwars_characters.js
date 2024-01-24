e7a49798827c96f999310415fcc80a172e578c3c
Aymen Idouahman <aymanidou@gmail.com>
2024-01-24 12:51:29 +0100

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const request = require('request');
+const urlFilm = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+// const urlPeople = 'https://swapi-api.alx-tools.com/api/people/';
+
+request(urlFilm, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const characters = JSON.parse(body).characters.sort();
+    console.log(characters);
+    // const people = {}
+    for (let index = 1; index < characters.length; index++) {
+      const element = characters[index];
+      request(element, function (err, res, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          // JSON.parse(body).results.forEach(people => {
+          console.log(JSON.parse(body).name);
+          // for (let index = 0; index < characters.length; index++) {
+          //   const element = characters[index];
+          //   if (element == people.url) {
+          //     console.log(people.name);
+          //   }
+          // }
+
+          // });
+        }
+      });
+    }
+  }
+});
